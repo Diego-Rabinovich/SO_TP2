9107879681b74010c1b022bd02915bd3cfbc4ca5
diegobadin <dbadin@itba.edu.ar>
2024-05-20 20:23:30 -0300

    Implementacion del gdb

--- x64barebones/run.sh
+++ x64barebones/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 1024
+if [[ "$1" = "gdb" ]]; then 
+  qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int 2>&1 | grep "v="
+else
+  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
