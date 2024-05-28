2e2b910407b90eb3de579c1e28019b92fd924968
Franco Ghigliani <fghigliani@itba.edu.ar>
2024-05-28 14:55:44 -0300

    Merge completo for some reason

--- x64barebones/Kernel/memoryManager.c
+++ x64barebones/Kernel/memoryManager.c
@@ -140,179 +140,124 @@ void memFree(void *ptr){
 }
 #else
 
-
-typedef struct Node {
+typedef struct Node{
+    unsigned long long size;
     Node* next;
     Node* prev;
-    unsigned long long size;
-} Node;
+}Node;
 
-Node* free_men;
-Node* used_mem;
+Node * free_list = NULL;
+Node * allocated_list = NULL;
 
-void memInit(void *start_ptr, unsigned long size_bytes){
-    if(size_bytes<MIN_BLOCK_SIZE){
-        return;
+void memInit(void* start_ptr, unsigned long size_bytes){
+    if(size_bytes < MIN_BLOCK_SIZE )
+        return NULL;
+
+
+    Node* aligned_start_ptr = (Node*)start_ptr;
+    if(((unsigned long long)aligned_start_ptr) % 8){
+        aligned_start_ptr = aligned_start_ptr + (8 - ((unsigned long long)aligned_start_ptr)%8);
     }
 
-    free_men=(Node *) ((start_ptr)%8==0? (start_ptr):(start_ptr + 8- (start_ptr)%8));
-    used_mem=NULL;
-    free_men->next=NULL;
-    free_men->prev=NULL;
-    free_men->size=size_bytes;
+    free_list = aligned_start_ptr;
+    free_list->next = NULL;
+    free_list->prev = NULL;
+    free_list->size = size_bytes;
 }
 
+void * memAllocInner(unsigned long bytes){
+    Node* current = free_list;
+    Node* best = NULL;
 
-void * memAlloc(unsigned long bytes){
-    if(bytes<MIN_BLOCK_SIZE){
-        return NULL;
-    }
-    unsigned long size = (bytes + sizeof(Node))%8==0? (bytes + sizeof(Node)):(bytes + sizeof(Node) + 8- (bytes + sizeof(Node)%8));
-
-    Node* current=free_men;
-    Node* best=NULL;
-    //buscamos el mejor lugar para alojar
-    while(current!=NULL){
-        if(best==NULL||current->size < best->size){
-            if(current->size>=size){
-                best=current;
-            }
+    while (current != NULL) //Busco el Best fit
+    {
+        if((best == NULL || best->size > current->size) && current->size >= bytes){
+            best = current;
         }
-        current=current->next;
-    }
-    if(best!=NULL){
-        //particionamos
-
-
-    }else{
-        //no hay lugar
-        return NULL;
+        current = current->next;
     }
 
+    //Encontre uno, tengo que partirlo
+    if(best != NULL){
+        //Creo el nuevo nodo resultado de la particion y lo pongo en la freelist
+        Node * newNode = best + bytes; //no uso sizeof(Node) porque bytes ya lo incluye
+        newNode->next = best->next;
+        newNode->prev = best->prev;
+        //Saco al best de free_list
+        if(newNode->prev != NULL)
+            newNode->prev->next = newNode;
+        if(newNode->next != NULL)
+            newNode->next->prev = newNode;
+
+        //Inserto best en allocated_list
+        best->prev = NULL;
+        best->next = allocated_list;
+        allocated_list->prev = best;
+        allocated_list = best;
+    }
+    return best;
+}
+
+void* memAlloc(unsigned long bytes){
+    bytes += sizeof(Node);  //Agrego el tamaño del header para que entre el header de lo que sobre
+                            //(aka el header del nuevo elemento de freelist)
+
+    if(bytes < MIN_BLOCK_SIZE)
+        bytes = MIN_BLOCK_SIZE;
+
+    if(bytes % 8){
+        bytes = bytes + (8 - bytes%8);  //Redondea a la proxima palabra
+    }
+
+    return memAllocInner(bytes);
+}
+
+void memFreeInner(Node *node){
+    //Saco a node de allocated_list
+    if(node->prev != NULL)
+        node->prev->next = node->next;
+    if(node->next != NULL)
+        node->next->prev = node->prev;
+
+
+    Node *current = free_list;
+    //Busco el nodo que predeceria a node en free_list (ordenado por address) (o caso borde, node va al comienzo)
+    while (current != NULL && current > node && current->next != NULL && current->next < node) {
+        current = current->next;
+    }
+    //Encontre uno
+    if(current != NULL && current < node){
+        node->prev = current;
+        node->next = current->next;
+        current->next = node;
+        if(current->next != NULL)
+            current->next->prev = node;
+    } else {
+        free_list = node;
+        node->prev = NULL;
+        node->next = current;
+    }
+
+    //Chequeamos si podemos mergear con nuestro next
+    if(node->next != NULL && node->next == node + sizeof(Node) + node->size){
+        node->size += sizeof(Node) + node->next->size;
+        node->next = node->next->next;
+        if(node->next->next != NULL){
+            node->next->next->prev = node;
+        }
+    }
+
+    //Chequeamos si podemos mergear con nuestro prev
+    if(node->prev != NULL && node == node->prev + sizeof(Node) + node->prev->size){
+        node->prev->size += sizeof(Node) + node->size;
+        node->prev->next = node->next;
+        if(node->next != NULL){
+            node->next->prev = node->prev;
+        }
+    }
 }
 
 void memFree(void *ptr){
-
+    memFreeInner((Node*)ptr - sizeof(Node));    //el ptr q me pasan esta a sizeof(Node) del comienzo del bloque
 }
-
-    typedef struct Node{
-        unsigned long long size;
-        Node* next;
-        Node* prev;
-    }Node;
-
-    Node * free_list = NULL;
-    Node * allocated_list = NULL;
-
-    void memInit(void* start_ptr, unsigned long size_bytes){
-        if(size_bytes < MIN_BLOCK_SIZE )
-            return NULL;
-
-
-        Node* aligned_start_ptr = (Node*)start_ptr;
-        if(((unsigned long long)aligned_start_ptr) % 8){
-            aligned_start_ptr = aligned_start_ptr + (8 - ((unsigned long long)aligned_start_ptr)%8);
-        }
-
-        free_list = aligned_start_ptr;
-        free_list->next = NULL;
-        free_list->prev = NULL;
-        free_list->size = size_bytes;
-    }
-
-    void * memAllocInner(unsigned long bytes){
-        Node* current = free_list;
-        Node* best = NULL;
-
-        while (current != NULL) //Busco el Best fit
-        {
-            if((best == NULL || best->size > current->size) && current->size >= bytes){
-                best = current;
-            }
-            current = current->next;
-        }
-
-        //Encontre uno, tengo que partirlo
-        if(best != NULL){
-            //Creo el nuevo nodo resultado de la particion y lo pongo en la freelist
-            Node * newNode = best + bytes; //no uso sizeof(Node) porque bytes ya lo incluye
-            newNode->next = best->next;
-            newNode->prev = best->prev;
-            //Saco al best de free_list
-            if(newNode->prev != NULL)
-                newNode->prev->next = newNode;
-            if(newNode->next != NULL)
-                newNode->next->prev = newNode;
-
-            //Inserto best en allocated_list
-            best->prev = NULL;
-            best->next = allocated_list;
-            allocated_list->prev = best;
-            allocated_list = best;
-        }
-        return best;
-    }
-
-    void* memAlloc(unsigned long bytes){
-        bytes += sizeof(Node);  //Agrego el tamaño del header para que entre el header de lo que sobre
-                                //(aka el header del nuevo elemento de freelist)
-
-        if(bytes < MIN_BLOCK_SIZE)
-            bytes = MIN_BLOCK_SIZE;
-
-        if(bytes % 8){
-            bytes = bytes + (8 - bytes%8);  //Redondea a la proxima palabra
-        }
-
-        return memAllocInner(bytes);
-    }
-
-    void memFreeInner(Node *node){
-        //Saco a node de allocated_list
-        if(node->prev != NULL)
-            node->prev->next = node->next;
-        if(node->next != NULL)
-            node->next->prev = node->prev;
-
-
-        Node *current = free_list;
-        //Busco el nodo que predeceria a node en free_list (ordenado por address) (o caso borde, node va al comienzo)
-        while (current != NULL && current > node && current->next != NULL && current->next < node) {
-            current = current->next;
-        }
-        //Encontre uno
-        if(current != NULL && current < node){
-            node->prev = current;
-            node->next = current->next;
-            current->next = node;
-            if(current->next != NULL)
-                current->next->prev = node;
-        } else {
-            free_list = node;
-            node->prev = NULL;
-            node->next = current;
-        }
-
-        //Chequeamos si podemos mergear con nuestro next
-        if(node->next != NULL && node->next == node + sizeof(Node) + node->size){
-            node->size += sizeof(Node) + node->next->size;
-            node->next = node->next->next;
-            if(node->next->next != NULL){
-                node->next->next->prev = node;
-            }
-        }
-
-        //Chequeamos si podemos mergear con nuestro prev
-        if(node->prev != NULL && node == node->prev + sizeof(Node) + node->prev->size){
-            node->prev->size += sizeof(Node) + node->size;
-            node->prev->next = node->next;
-            if(node->next != NULL){
-                node->next->prev = node->prev;
-            }
-        }
-    }
-
-    void memFree(void *ptr){
-        memFreeInner((Node*)ptr - sizeof(Node));    //el ptr q me pasan esta a sizeof(Node) del comienzo del bloque
-    }
 #endif
